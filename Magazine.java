276f85602411efd1aaddefc236674cb081a60cb7
adonayaraia02@gmail.com <adonayaraia02@gmail.com>
2025-02-18 13:57:41 -0500

    double updating

--- Magazine.java
+++ Magazine.java
@@ -1,63 +1,69 @@
-public class Magazine{
+public class Magazine {
     // instance variables
     private String title;
-    private double cost ;
+    private double cost;
     private int numInStock;
 
-    // Constructors 
-    // makes the code flexible 
-    Magazine(){
-    this.title = "";
-    this.cost = 0;
-    this.numInStock = 0;
+    // Constructors
+    // makes the code flexible
+    Magazine() {
+        this.title = "";
+        this.cost = 0;
+        this.numInStock = 0;
     }
-    Magazine(String title, double cost ){
+
+    Magazine(String title, double cost) {
         this.title = title;
         this.cost = cost;
     }
 
-    Magazine( String title, double cost, int numInStock){
+    Magazine(String title, double cost, int numInStock) {
         this.title = title;
         this.cost = cost;
         this.numInStock = numInStock;
     }
 
-    //Accessors
-
-    public String getTitle(){
+    // Accessors
+    public String getTitle() {
         return title;
     }
 
-    public double getCost(){
+    public double getCost() {
         return cost;
     }
 
-    public int getNumInStco(){
+    public int getNumInStco() {
         return numInStock;
     }
 
-    //Mutator
+    // Mutator
 
-    public void setTitle(String title){
-        if (!title.equals( ""))
+    public void setTitle(String title) {
+        if (!title.equals(""))
             this.title = title;
+        else
+            throw new IllegalArgumentException("the magazine title can not be empty");
     }
 
-    public void setCost( double cost ){
-        if ( cost > 0)
-        this.cost = cost;
+    public void setCost(double cost) {
+        if (cost > 0)
+            this.cost = cost;
+        else
+            throw new IllegalArgumentException("the price can't be negative");
     }
 
-    public void setNumInStock(int numInStock){
-        if( numInStock > 0)
-        this.numInStock = numInStock;
+    public void setNumInStock(int numInStock) {
+        if (numInStock > 0)
+            this.numInStock = numInStock;
+        else
+            throw new IllegalArgumentException("The Stock can't be negative");
     }
 
     // toString Method
-    public String toString( ){
-        return "Title " + this.getTitle() + 
-        "\n" + "Cost " + this.getCost()+
-        "\n" + "Stock " + this.getNumInStco();
+    public String toString() {
+        return "Title " + this.getTitle() +
+                "\n" + "Cost " + this.getCost() +
+                "\n" + "Stock " + this.getNumInStco();
     }
 
 }
--- MagazineImp.java
+++ MagazineImp.java
@@ -2,25 +2,50 @@ import javax.swing.JOptionPane;
 
 public class MagazineImp {
     public static void main(String[] args) {
-        
+
+        double total = 0;
+
         Magazine m1 = new Magazine("Sport Illustrated ", 15);
-        JOptionPane.showMessageDialog(null,  m1.toString());
+        total += m1.getCost();
+        JOptionPane.showMessageDialog(null, m1.toString());
 
         Magazine m2 = new Magazine("Animals", 20, 5);
-        JOptionPane.showMessageDialog(null,  m2.toString());
-        
+        total += m2.getCost();
+        JOptionPane.showMessageDialog(null, m2.toString());
+
         Magazine m3 = new Magazine();
-        String  title = JOptionPane.showInputDialog("Enter the tile");
-        double  cost = Double.parseDouble(JOptionPane.showInputDialog("Enter the cost"));
-        int numInStock = Integer.parseInt(JOptionPane.showInputDialog("Enter the Number of stocks"));
 
-        m3.setTitle(title);
-        m3.setCost(cost);
-        m3.setNumInStock(numInStock);
+        // get title from user input and set it
+        try {
+            String title = JOptionPane.showInputDialog("Enter the tile");
+            m3.setTitle(title);
+        } catch (IllegalArgumentException er) {
+            JOptionPane.showMessageDialog(null, er.getMessage());
+        }
 
-       
-        JOptionPane.showMessageDialog(null,  m3.toString());
+        // get user input from price and set it
+        try {
+            double cost = Double.parseDouble(JOptionPane.showInputDialog("Enter the cost"));
+            m3.setCost(cost);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "the cost must be a number");
+        } catch (IllegalArgumentException er) {
+            JOptionPane.showMessageDialog(null, er.getMessage());
+        }
+    
+        // get user input from stock and set it
+        try {
+            int numInStock = Integer.parseInt(JOptionPane.showInputDialog("Enter the Number of stocks"));
+            m3.setNumInStock(numInStock);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "the stock must be a number");
+        } catch (IllegalArgumentException er) {
+            JOptionPane.showMessageDialog(null, er.getMessage());
+        }
+        total += m3.getCost();
+        JOptionPane.showMessageDialog(null, m3.toString());
+        JOptionPane.showMessageDialog(null, "The avarage cost over all the magazine $ " + (total / 3));
 
     }
-    
+
 }
